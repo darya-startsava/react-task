78451ad7ce8f1bd19b2c11e6a9105fb3507f09b1
darya-startsava <daryastartsava@gmail.com>
2022-12-15 16:57:46 +0100

    feat: add eslint and prettier

--- src/components/textarea/Textarea.jsx
+++ src/components/textarea/Textarea.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 import './Textarea.css';
 
 export default class Textarea extends React.Component {
   render() {
     const { label, rows, placeholder } = this.props;
     return (
-      <label>{label}<textarea className="textarea_text" rows={rows} placeholder={placeholder} /></label>
+      <label>
+        {label}
+        <textarea className="textarea_text" rows={rows} placeholder={placeholder} />
+      </label>
     );
   }
 }
